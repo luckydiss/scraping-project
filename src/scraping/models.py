fea3cb087c4c442c67da17b3c98bcaab61d2c935
luckydiss <babayka709@gmail.com>
2022-07-18 15:10:22 +0300

    Добавил модель Language

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -5,11 +5,23 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=50,
                             verbose_name='Название населенного пункта')
-    plug = models.CharField(max_length=50, blank=True)
+    plug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
         verbose_name = 'Название населенного пункта'
         verbose_name_plural = 'Название населенных пунктов'
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования')
+    plug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
 
     def __str__(self):
         return self.name
