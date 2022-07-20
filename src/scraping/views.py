6a870e2275bb25b552aa5c7f4b4ee6639bdf0ee5
luckydiss <babayka709@gmail.com>
2022-07-20 07:56:58 +0300

    Передача параметров во вьюху

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -4,6 +4,16 @@ from .models import Vacancy
 # Create your views here.
 
 def home_view(request):
-    qs = Vacancy.objects.all()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        filter_ = {}
+        if city:
+            filter_['city__name'] = city
+        if language:
+            filter_['language__name'] = language
+
+        qs = Vacancy.objects.filter(**filter_)
     return render(request, 'home.html', {'object_list': qs})
 
